91e9d79881a4fcacad2c33b490bf558f244701b0
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-07-16 00:39:19 +0200

    [ADD]: New confession page (empty for now)

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,16 +1,36 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import Feather from '@expo/vector-icons/Feather';
+import AntDesign from '@expo/vector-icons/AntDesign';
 import { Tabs } from 'expo-router'
 
 const TabsLayout = () => {
     return (
         <Tabs screenOptions={{
-            tabBarActiveBackgroundColor: "#183431"
+            tabBarActiveBackgroundColor: "#183431",
+            tabBarInactiveBackgroundColor: "#183431",
         }}>
-            <Tabs.Screen name="feed" options={{
-                headerShown: false,
-                tabBarLabel: "Feed"
-            }} />
+            <Tabs.Screen
+                name="feed"
+                options={{
+                    headerShown: false,
+                    tabBarLabel: "",
+                    tabBarIcon: ({ size }) => (
+                        <Feather name="book-open" size={size} color={"white"} />
+                    )
+                }}
+            />
+
+
+            <Tabs.Screen
+                name="new"
+                options={{
+                    headerShown: false,
+                    tabBarLabel: "",
+                    tabBarIcon: ({ size }) => (
+                        <AntDesign name="pluscircleo" size={size} color="white" />
+                    )
+                }}
+            />
         </Tabs>
     )
 }
---
+++ app/(tabs)/new/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native"
+
+function NewConfession() {
+    return (
+        <View>
+            <Text>Create new confession</Text>
+        </View>
+    )
+}
+
+export default NewConfession
