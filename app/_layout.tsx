b439952d6a8351f65eb41092f241dc56bd7cede7
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-07-15 22:24:47 +0200

    [ADD]: NativeWindUI dependencies

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,29 @@
 import '../global.css';
+import 'expo-dev-client';
+
+import { StatusBar } from 'expo-status-bar';
 
 import { Stack } from 'expo-router';
+import { ThemeProvider as NavThemeProvider } from '@react-navigation/native';
+
+import { useColorScheme, useInitialAndroidBarSync } from '~/lib/useColorScheme';
+import { NAV_THEME } from '~/theme';
 
 export default function Layout() {
-  return <Stack />;
+  useInitialAndroidBarSync();
+  const { colorScheme, isDarkColorScheme } = useColorScheme();
+
+  return (
+    <>
+      <StatusBar
+        key={`root-status-bar-${isDarkColorScheme ? 'light' : 'dark'}`}
+        style={isDarkColorScheme ? 'light' : 'dark'}
+      />
+
+      <NavThemeProvider value={NAV_THEME[colorScheme]}>
+        <Stack />
+      </NavThemeProvider>
+    </>
+
+  )
 }
